3efb0fdfb63cb60739bc515373c44fc10cbb379f
John Bedell <jbedell@newrelic.com>
2024-01-26 15:50:37 -0500

    Implemented the titan "embed" models. Restructure the code to reduce some duplication.

--- src/main/java/com/demo/awsbedrockspringboot/awsbedrockruntime/InvokeModel.java
+++ src/main/java/com/demo/awsbedrockspringboot/awsbedrockruntime/InvokeModel.java
@@ -5,11 +5,15 @@ import org.json.JSONObject;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeAsyncClient;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class InvokeModel {
 
@@ -19,22 +23,15 @@ public class InvokeModel {
      * @param prompt The prompt for Claude to complete.
      * @return The generated response.
      */
-    public static String invokeClaude(String prompt) {
+    public static String invokeClaude(String prompt, Boolean async) {
         /*
           The different model providers have individual request and response formats.
           For the format, ranges, and default values for Anthropic Claude, refer to:
           https://docs.anthropic.com/claude/reference/complete_post
          */
 
-        String claudeModelId = "anthropic.claude-v2";
-
         // Claude requires you to enclose the prompt as follows:
         String enclosedPrompt = "Human: " + prompt + "\n\nAssistant:";
-
-        BedrockRuntimeClient client = BedrockRuntimeClient.builder()
-                .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
 
         String payload = new JSONObject()
                 .put("prompt", enclosedPrompt)
@@ -43,18 +40,7 @@ public class InvokeModel {
                 .put("stop_sequences", List.of("\n\nHuman:"))
                 .toString();
 
-        InvokeModelRequest request = InvokeModelRequest.builder()
-                .body(SdkBytes.fromUtf8String(payload))
-                .modelId(claudeModelId)
-                .contentType("application/json")
-                .accept("application/json")
-                .build();
-
-        InvokeModelResponse response = client.invokeModel(request);
-
-        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
-
-        return responseBody.getString("completion");
+        return invokeAndGetString(AwsBedrockRuntimeHelper.CLAUDE, payload, "completion", async);
     }
 
     /**
@@ -63,19 +49,12 @@ public class InvokeModel {
      * @param prompt The prompt for Jurassic to complete.
      * @return The generated response.
      */
-    public static String invokeJurassic2(String prompt) {
+    public static String invokeJurassic2(String prompt, Boolean async) {
         /*
           The different model providers have individual request and response formats.
           For the format, ranges, and default values for AI21 Labs Jurassic-2, refer to:
           https://docs.ai21.com/reference/j2-complete-ref
          */
-
-        String jurassic2ModelId = "ai21.j2-mid-v1";
-
-        BedrockRuntimeClient client = BedrockRuntimeClient.builder()
-                .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
 
         String payload = new JSONObject()
                 .put("prompt", prompt)
@@ -83,24 +62,8 @@ public class InvokeModel {
                 .put("maxTokens", 200)
                 .toString();
 
-        InvokeModelRequest request = InvokeModelRequest.builder()
-                .body(SdkBytes.fromUtf8String(payload))
-                .modelId(jurassic2ModelId)
-                .contentType("application/json")
-                .accept("application/json")
-                .build();
-
-        InvokeModelResponse response = client.invokeModel(request);
-
-        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
-
-        String generatedText = responseBody
-                .getJSONArray("completions")
-                .getJSONObject(0)
-                .getJSONObject("data")
-                .getString("text");
-
-        return generatedText;
+        return invokeAndGetJSONArray(AwsBedrockRuntimeHelper.JURASSIC2, payload, "completions", async)
+                .getJSONObject(0).getJSONObject("data").getString("text");
     }
 
     /**
@@ -109,19 +72,12 @@ public class InvokeModel {
      * @param prompt The prompt for Llama 2 to complete.
      * @return The generated response.
      */
-    public static String invokeLlama2(String prompt) {
+    public static String invokeLlama2(String prompt, Boolean async) {
         /*
           The different model providers have individual request and response formats.
           For the format, ranges, and default values for Meta Llama 2 Chat, refer to:
           https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-meta.html
          */
-
-        String llama2ModelId = "meta.llama2-13b-chat-v1";
-
-        BedrockRuntimeClient client = BedrockRuntimeClient.builder()
-                .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
 
         String payload = new JSONObject()
                 .put("prompt", prompt)
@@ -130,20 +86,7 @@ public class InvokeModel {
                 .put("top_p", 0.9)
                 .toString();
 
-        InvokeModelRequest request = InvokeModelRequest.builder()
-                .body(SdkBytes.fromUtf8String(payload))
-                .modelId(llama2ModelId)
-                .contentType("application/json")
-                .accept("application/json")
-                .build();
-
-        InvokeModelResponse response = client.invokeModel(request);
-
-        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
-
-        String generatedText = responseBody.getString("generation");
-
-        return generatedText;
+        return invokeAndGetString(AwsBedrockRuntimeHelper.LLAMA2, payload, "generation", async);
     }
 
     /**
@@ -154,19 +97,12 @@ public class InvokeModel {
      * @param stylePreset The style preset to guide the image model towards a specific style.
      * @return A Base64-encoded string representing the generated image.
      */
-    public static String invokeStableDiffusion(String prompt, long seed, String stylePreset) {
+    public static String invokeStableDiffusion(String prompt, long seed, String stylePreset, Boolean async) {
         /*
           The different model providers have individual request and response formats.
           For the format, ranges, and available style_presets of Stable Diffusion models refer to:
           https://platform.stability.ai/docs/api-reference#tag/v1generation
          */
-
-        String stableDiffusionModelId = "stability.stable-diffusion-xl";
-
-        BedrockRuntimeClient client = BedrockRuntimeClient.builder()
-                .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
 
         JSONArray wrappedPrompt = new JSONArray().put(new JSONObject().put("text", prompt));
 
@@ -178,23 +114,8 @@ public class InvokeModel {
             payload.put("style_preset", stylePreset);
         }
 
-        InvokeModelRequest request = InvokeModelRequest.builder()
-                .body(SdkBytes.fromUtf8String(payload.toString()))
-                .modelId(stableDiffusionModelId)
-                .contentType("application/json")
-                .accept("application/json")
-                .build();
-
-        InvokeModelResponse response = client.invokeModel(request);
-
-        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
-
-        String base64ImageData = responseBody
-                .getJSONArray("artifacts")
-                .getJSONObject(0)
-                .getString("base64");
-
-        return base64ImageData;
+        return invokeAndGetJSONArray(AwsBedrockRuntimeHelper.STABLE_DIFFUSION, payload.toString(), "artifacts", async)
+                .getJSONObject(0).getString("base64");
     }
 
     /**
@@ -205,18 +126,12 @@ public class InvokeModel {
      * @param seed   The random noise seed for image generation (Range: 0 to 2147483647).
      * @return A Base64-encoded string representing the generated image.
      */
-    public static String invokeTitanImage(String prompt, long seed) {
+    public static String invokeTitanImage(String prompt, long seed, Boolean async) {
         /*
          The different model providers have individual request and response formats.
          For the format, ranges, and default values for Titan Image models refer to:
          https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-titan-image.html
         */
-        String titanImageModelId = "amazon.titan-image-generator-v1";
-
-        BedrockRuntimeClient client = BedrockRuntimeClient.builder()
-                .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
 
         var textToImageParams = new JSONObject().put("text", prompt);
 
@@ -228,27 +143,130 @@ public class InvokeModel {
                 .put("width", 512)
                 .put("seed", seed);
 
-        JSONObject payload = new JSONObject()
+        String payload = new JSONObject()
                 .put("taskType", "TEXT_IMAGE")
                 .put("textToImageParams", textToImageParams)
-                .put("imageGenerationConfig", imageGenerationConfig);
+                .put("imageGenerationConfig", imageGenerationConfig)
+                .toString();
 
+        return invokeAndGetJSONArray(AwsBedrockRuntimeHelper.TITAN_IMAGE, payload, "images", async)
+                .getString(0);
+    }
+
+    /**
+     * Invokes the Amazon Titan embed text generation model to run an inference based on the provided input.
+     *
+     * @param prompt The prompt that you want Amazon Titan to use for embed generation.
+     * @return The generated response.
+     */
+    public static String invokeTitanEmbedText(String prompt, Boolean async) {
+        /*
+         The different model providers have individual request and response formats.
+         For the format, ranges, and default values for Titan Embed Text models refer to:
+         https://docs.aws.amazon.com/bedrock/latest/userguide/titan-embedding-models.html
+        */
+
+        String payload = new JSONObject()
+                .put("inputText", prompt)
+                .toString();
+
+        return invokeAndGetJSONArray(AwsBedrockRuntimeHelper.TITAN_EMBED_TEXT, payload, "embedding", async)
+                .toList().toString();
+    }
+
+    /**
+     * Invokes the Amazon Titan embed image generation model to create an image using the input
+     * provided in the request body.
+     *
+     * @param prompt The prompt that you want Amazon Titan to use for image generation.
+     * @return A Base64-encoded string representing the generated image.
+     */
+    public static String invokeTitanEmbedImage(String prompt, Boolean async) {
+        /*
+         The different model providers have individual request and response formats.
+         For the format, ranges, and default values for Titan Embed Image models refer to:
+         https://docs.aws.amazon.com/bedrock/latest/userguide/titan-multiemb-models.html
+        */
+
+        JSONObject embeddingConfig = new JSONObject()
+                .put("outputEmbeddingLength", 256);  // 256, 384 or 1024
+
+        String payload = new JSONObject()
+                .put("inputText", prompt)
+                //.put("inputImage", null)
+                .put("embeddingConfig", embeddingConfig)
+                .toString();
+
+        return invokeAndGetJSONArray(AwsBedrockRuntimeHelper.TITAN_EMBED_IMAGE, payload, "embedding", async)
+                .toList().toString();
+    }
+
+    private static JSONArray invokeAndGetJSONArray(String modelId, String payload, String fieldName, Boolean async) {
+        InvokeModelResponse response = async ?
+                invokeAsyncAndGetResponse(modelId, payload) : invokeSyncAndGetResponse(modelId, payload);
+        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
+        return responseBody.getJSONArray(fieldName);
+    }
+
+    private static String invokeAndGetString(String modelId, String payload, String fieldName, Boolean async) {
+        InvokeModelResponse response = async ?
+                invokeAsyncAndGetResponse(modelId, payload) : invokeSyncAndGetResponse(modelId, payload);
+        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
+        return responseBody.getString(fieldName);
+    }
+
+    private static InvokeModelResponse invokeSyncAndGetResponse(String modelId, String payload) {
+        System.out.println("Invoking synchronously");
         InvokeModelRequest request = InvokeModelRequest.builder()
-                .body(SdkBytes.fromUtf8String(payload.toString()))
-                .modelId(titanImageModelId)
+                .body(SdkBytes.fromUtf8String(payload))
+                .modelId(modelId)
                 .contentType("application/json")
                 .accept("application/json")
                 .build();
 
-        InvokeModelResponse response = client.invokeModel(request);
+        return getClientSync().invokeModel(request);
+    }
 
-        JSONObject responseBody = new JSONObject(response.body().asUtf8String());
+    private static BedrockRuntimeClient getClientSync() {
+        return BedrockRuntimeClient.builder()
+                .region(Region.US_WEST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
 
-        String base64ImageData = responseBody
-                .getJSONArray("images")
-                .getString(0);
+    private static InvokeModelResponse invokeAsyncAndGetResponse(String modelId, String payload) {
+        System.out.println("Invoking asynchronously");
+        InvokeModelRequest request = InvokeModelRequest.builder()
+                .body(SdkBytes.fromUtf8String(payload))
+                .modelId(modelId)
+                .contentType("application/json")
+                .accept("application/json")
+                .build();
 
-        return base64ImageData;
+        CompletableFuture<InvokeModelResponse> completableFuture = getClientAsync().invokeModel(request)
+                .whenComplete((response, exception) -> {
+                    if (exception != null) {
+                        System.out.println("Model invocation failed: " + exception);
+                    }
+                });
+
+        InvokeModelResponse response = null;
+        try {
+            response = completableFuture.get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println(e.getMessage());
+        } catch (ExecutionException e) {
+            System.err.println(e.getMessage());
+        }
+        return response;
+    }
+
+    private static BedrockRuntimeAsyncClient getClientAsync() {
+        return BedrockRuntimeAsyncClient.builder()
+                .region(Region.US_WEST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
     }
 
 }
